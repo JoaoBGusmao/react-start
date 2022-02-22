7254f844a232fbb07d0643013ea10c36bd689701
João Gusmão <joao@gusmao.dev>
2022-02-22 00:21:22 +0000

    Base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Example from './Components/Example';
+import './Assets/css/app.sass';
+
+const App = () => (
+  <div className="App">
+    <h1>React!</h1>
+
+    <Example />
+  </div>
+);
+
+export default App;
---
+++ src/Components/Example.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Example.sass';
+
+const Example = () => (
+  <div className="Example">
+    <p className="Example__text">Componente exemplo</p>
+  </div>
+);
+
+export default Example;
